06fd3f00273938438fcfb14fabc4fdaa18be783c
vnmatt <gordon_matt@hotmail.com>
2018-06-02 12:15:37 +0930

    Attempt bundling

---
+++ Aurelia.Skeleton.NetCore.Razor/gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var bundle = require('aurelia-bundler').bundle;
+
+var config = {
+    force: true,
+    baseURL: './wwwroot', // baseURL of the application
+    configPath: './wwwroot/config.js', // config.js file. Must be within `baseURL`
+    bundles: {
+        //"dist/app-build": {
+        //    includes: [
+        //        '[aurelia-app/*.js]',
+        //        '[aurelia-app/**/*.js]',
+        //        '[*.js]'
+        //    ],
+        //    excludes: [
+        //        'config.js'
+        //    ],
+        //    options: {
+        //        inject: true,
+        //        minify: true
+        //    }
+        //},
+        "dist/vendor-build": {
+            includes: [
+                'aurelia-binding',
+                'aurelia-bootstrapper',
+                'aurelia-dependency-injection',
+                //'aurelia-dynamic-html',
+                'aurelia-event-aggregator',
+                'aurelia-framework',
+                'aurelia-history',
+                'aurelia-http-client',
+                'aurelia-kendoui-bridge',
+                'aurelia-loader',
+                'aurelia-loader-default',
+                'aurelia-logging',
+                'aurelia-metadata',
+                'aurelia-pal',
+                'aurelia-pal-browser',
+                'aurelia-path',
+                'aurelia-route-recognizer',
+                'aurelia-router',
+                'aurelia-task-queue',
+                'aurelia-templating',
+                'aurelia-templating-resources',
+                'aurelia-templating-router',
+                'aurelia-tinymce-wrapper',
+                'nprogress',
+                //'text',
+                'tinymce',//,
+                //'bootstrap/css/bootstrap.css!text'
+            ],
+            options: {
+                inject: true,
+                minify: true
+            }
+        }
+    }
+};
+
+gulp.task('bundle', function () {
+    return bundle(config);
+});
